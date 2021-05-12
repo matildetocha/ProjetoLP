04cc8cf1a83c159a774f482ffc1a6659fb58ef3a
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2021-05-12 17:26:01 +0100

    predicados 3, 4 e 11

--- kakuro_puzzle.pl
+++ kakuro_puzzle.pl
@@ -1,27 +1,157 @@
 %% Matilde Tocha 99108
 
 :- [codigo_comum].
-combinacoes(N, Els, Combs) :-
-    findall(Comb, combinacao(N, Els, Comb), Combs).
 
 % ----------------------------------------------------------------------
 %                           combinacoes_soma/4
+% Significa que Combs eh a lista ordenada cujos elementos sao as combinacoes 
+% N a N, dos elementos de Els cuja soma e Soma.
 % ----------------------------------------------------------------------
+
 combinacoes_soma(N, Els, Soma, Combs) :-
-    combinacoes(N, Els, Aux),
-    findall([El1, El2], 
-        (member(Mem, Aux), nth0(0, Mem, El1), nth0(1, Mem, El2), Soma is El1 + El2), 
+    findall(Comb, 
+        (combinacao(N, Els, Comb), foldl(plus, Comb, 0, Soma_Aux), Soma_Aux =:= Soma), 
         Combs).
 
 % ----------------------------------------------------------------------
 %                           permutacoes_soma/4
+% Significa que Perms eh a lista ordenada cujos elementos sao as permutacoes
+% das combinacoes N a N, dos elementos de Els cuja soma eh Soma.
 % ----------------------------------------------------------------------
 permutacoes_soma(N, Els, Soma, Perms) :-
-    combinacoes(N, Els, Aux),
-    findall(Perm, (member(Mem, Aux), permutation(Mem, Perm)), Aux2),
-    findall([El1, El2], 
-        (member(Mem, Aux2), nth0(0, Mem, El1), nth0(1, Mem, El2), Soma is El1 + El2), 
+    findall(Perm, (combinacao(N, Els, Comb), permutation(Comb, Perm)), Aux),
+    findall(Perm, 
+        (member(Perm, Aux), foldl(plus, Perm, 0, Soma_Aux), Soma_Aux =:= Soma), 
         Res),
     sort(Res, Perms).
 
-  
+% ----------------------------------------------------------------------
+%                         pos_listas_Fila/2
+% Predicado Auxiliar.
+% Significa que Posicoes eh a lista de listas [pos, lista], significando
+% que essas listas contem as posicoes dos elementos da Fila que sejam listas.
+% ----------------------------------------------------------------------
+pos_listas_Fila(Fila, Posicoes) :-
+    length(Fila, N),
+    findall([Pos, Candidato], 
+        (between(1, N, Pos), nth1(Pos, Fila, Candidato), is_list(Candidato)),
+        Posicoes).
+
+% ----------------------------------------------------------------------
+%                         espaco/4
+% Predicado Auxiliar.
+% Significa que Vars_Esp eh a lista cujos elementos sao as variaveis de um 
+% espaco.
+% ----------------------------------------------------------------------
+espaco(Fila, Posicoes, Pos_Somas, Vars_Esp) :- 
+    espaco(Fila, Posicoes, Pos_Somas, Vars_Esp, [], []).
+
+espaco(_, [], Aux_Somas, Aux, Aux_Somas, Aux).
+
+espaco(Fila, [[_, Lst2]], Pos_Somas, Vars_Esp, Aux_Somas, Aux) :- 
+    length(Fila, Max), nth1(Max, Fila, Lst1), Lst1 == Lst2,
+    espaco(Fila, [], Pos_Somas, Vars_Esp, Aux_Somas, Aux).
+
+espaco(Fila, [[El, _]], Pos_Somas, Vars_Esp, Aux_Somas, Aux) :- 
+    length(Fila, Max), Min is El + 1,
+    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
+    espaco(Fila, [], Pos_Somas, Vars_Esp, [El | Aux_Somas] ,[Vars | Aux]).
+
+espaco(Fila, [El1, El2 | R], Pos_Somas, Vars_Esp, Aux_Somas, Aux) :-
+    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
+    Min is Pos1 + 1, Max is Pos2 - 1,
+    Min < Max,
+    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
+    append([El2], R, New_R),
+    espaco(Fila, New_R, Pos_Somas, Vars_Esp, [Pos1 | Aux_Somas] ,[Vars | Aux]).
+
+espaco(Fila, [El1, El2 | R], Pos_Somas, Vars_Esp, Aux_Somas, Aux) :-
+    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
+    Min is Pos1 + 1, Max is Pos2 - 1,
+    Min >= Max,
+    append([El2], R, New_R),
+    espaco(Fila, New_R, Pos_Somas, Vars_Esp, Aux_Somas, Aux).
+
+% ----------------------------------------------------------------------
+%                           somas_H_V/2
+% Significa Somas eh uma lista cujos elementos sao as somas da fila, 
+% conforme se trate de uma fila horizontal ou vertical.
+% ----------------------------------------------------------------------
+somas_H_V(H_V, Posicoes, Fila, Somas) :-
+    H_V == v,
+    findall(Soma, 
+        (member(Pos, Posicoes), nth1(Pos, Fila, Somas), nth0(0, Somas, Soma)), 
+        Somas).
+
+somas_H_V(H_V, Posicoes, Fila, Somas) :-
+    H_V == h,
+    findall(Soma, 
+        (member(Pos, Posicoes), nth1(Pos, Fila, Somas), nth0(1, Somas, Soma)), 
+        Somas).
+
+% ----------------------------------------------------------------------
+%                           espaco_fila/2
+% Significa que Esp eh um espaco de Fila.
+% ----------------------------------------------------------------------
+espaco_fila(Fila, Esp, H_V) :-
+    pos_listas_Fila(Fila, Posicoes),
+    espaco(Fila, Posicoes, Pos_Somas, Vars), 
+    length(Vars, N), sort(Vars, Vars_Sorted), sort(Pos_Somas, Pos_Somas_Sorted),
+    somas_H_V(H_V, Pos_Somas_Sorted, Fila, Somas),
+    between(1, N, Indice), 
+    nth1(Indice, Vars_Sorted, Espaco), nth1(Indice, Somas, Soma), 
+    Esp = espaco(Soma, Espaco).
+
+% ----------------------------------------------------------------------
+%                           espacos_fila/2
+% Significa que Espacos eh a lista de todos os espacos de Fila, da esquerda 
+% para a direita.
+% ----------------------------------------------------------------------
+espacos_fila(H_V, Fila, []) :-
+    \+ espaco_fila(Fila, _, H_V), !.
+
+espacos_fila(H_V, Fila, Espacos) :-
+    bagof(Esp, espaco_fila(Fila, Esp, H_V), Espacos).
+
+% ----------------------------------------------------------------------
+%                           espacos_puzzle/2
+% Significa que Espacos eh a lista de todos os espacos de Puzzle.
+% ----------------------------------------------------------------------
+
+% ----------------------------------------------------------------------
+%                           percorre_lst_perms/2
+% Predicado Auxiliar.
+% Significa que Comuns eh a lista de listas [pos, numero], significando 
+% que as listas verificadas contem o numero "numero" na posicao "pos".
+% ----------------------------------------------------------------------
+percorre_lst_perms(P, Perm, Comuns) :-
+    length(P, N),
+    findall([Pos, Num], (between(1, N, Pos), nth1(Pos, P, Num), nth1(Pos, Perm, Candidato),
+    Num =:= Candidato), Comuns).
+
+% ----------------------------------------------------------------------
+%                           comuns/2
+%                      Predicado Auxiliar
+% Significa que Res eh a lista que contem todos os Comuns.
+% ----------------------------------------------------------------------
+comuns([P | Perms], Res) :-
+    findall(Comuns, (member(Perm, Perms), percorre_lst_perms(P, Perm, Comuns), Comuns \= []), Res).
+
+% ----------------------------------------------------------------------
+%                           numeros_comuns/2
+% Significa que Numeros_Comuns eh uma lista de pares (pos, numero), significando
+% que todas as listas de Lst_Perms contem o numero "numero" na posicao "pos".
+% ----------------------------------------------------------------------
+numeros_comuns(Lst_Perms, Numeros_comuns) :-
+    comuns(Lst_Perms, Aux),
+    length(Aux, N), N < 2,     
+    Numeros_comuns = [], !.
+
+numeros_comuns(Lst_Perms, Numeros_comuns) :-
+    comuns(Lst_Perms, Aux),    
+    nth0(0, Aux, P),
+    findall((Pos, Num), 
+    (member(Perm, Aux), P == Perm, member(Comuns, P), nth0(0, Comuns, Pos), nth0(1, Comuns, Num)), Res),
+    list_to_set(Res, Numeros_comuns).
+
+    
--- teste.pl
+++ teste.pl
@@ -1,17 +1,94 @@
-:- [codigo_comum].
+%% -------------------------------------------------------
+aux([P | R], [P | Res]) :-
+    var(P),
+    aux(R, Res).
 
-combinacoes_soma(0, _, _, []).
+aux([P | _], []) :- is_list(P), !.
 
-combinacoes_soma(N, Els, Soma, [Comb | Combs]) :-
-    combinacao(N, Els, Comb),
-    nth0(0, Comb, El1),
-    nth0(1, Comb, El2),
-    El1 + El2 =:= Soma,
-    combinacoes_soma(N, Els, Soma, Combs).
+variaveis([P | R], H_V, A, L) :- 
+    var(P), 
+    variaveis(R, H_V, A, L).
 
-combinacoes_soma(N, Els, Soma, Combs) :-
-    combinacao(N, Els, Comb),
-    nth0(0, Comb, El1),
-    nth0(1, Comb, El2),
-    El1 + El2 =\= Soma,
-    combinacoes_soma(N, Els, Soma, Combs).
+variaveis([P | R], _, [], _) :- 
+    is_list(P), 
+    aux(R, Aux),
+    length(Aux, N), N == 0.
+
+variaveis([P | R], H_V, Aux, V) :- 
+    is_list(P), 
+    aux(R, Aux),
+    length(Aux, N), N > 0,
+    H_V == v, nth0(0, P, V).
+
+variaveis([P | R], H_V, Aux, H) :- 
+    is_list(P), 
+    aux(R, Aux),
+    length(Aux, N), N > 0,
+    H_V == h, nth0(1, P, H).
+
+espaco_fila(Fila, [], H_V) :-
+    variaveis(Fila, H_V, Aux, _),
+    Aux == [].
+
+espaco_fila(Fila, Esp, H_V) :-
+    variaveis(Fila, H_V, Aux, L),
+    Esp = espaco(L, Aux).
+
+%% ---------------------------------------------------------
+
+pos_variaveis(Fila, Posicoes) :-
+    length(Fila, N),
+    findall([Pos, Candidato], 
+    (between(1, N, Pos), nth1(Pos, Fila, Candidato), is_list(Candidato)),
+    Posicoes).
+
+espaco(Fila, Posicoes, Res) :- espaco(Fila, Posicoes, Res, []).
+
+espaco(_, [], Aux, Aux).
+
+espaco(Fila, [[El, _]], Res, Aux) :- 
+    length(Fila, Max), Min is El + 1,
+    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
+    espaco(Fila, [], Res, [Vars | Aux]).
+
+espaco(Fila, [El1, El2 | R], Res, Aux) :-
+    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
+    Min is Pos1 + 1, Max is Pos2 - 1,
+    Min < Max,
+    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
+    append([El2], R, New_R),
+    espaco(Fila, New_R, Res, [Vars | Aux]).
+
+espaco(Fila, [El1, El2 | R], Res, Aux) :-
+    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
+    Min is Pos1 + 1, Max is Pos2 - 1,
+    Min >= Max,
+    append([El2], R, New_R),
+    espaco(Fila, New_R, Res, Aux).
+
+espacos_fila(H_V, Fila, Espacos) :-
+    pos_variaveis(Fila, Posicoes),
+    espaco(Fila, Posicoes, Vars), length(Vars, N), sort(Vars, Vars_Sorted),
+    H_V == v,
+    findall(Soma, (member(Mem, Posicoes), nth0(1, Mem, Somas), nth0(0, Somas, Soma), Soma =\= 0), Somas_v),
+    bagof(espaco(Soma, Espaco), Indice^(between(1, N, Indice), nth1(Indice, Vars_Sorted, Espaco), nth1(Indice, Somas_v, Soma)), Espacos), !.
+
+espacos_fila(H_V, Fila, Espacos) :-
+    pos_variaveis(Fila, Posicoes),
+    espaco(Fila, Posicoes, Vars), length(Vars, N), sort(Vars, Vars_Sorted),
+    H_V == h,
+    findall(Soma, (member(Mem, Posicoes), nth0(1, Mem, Somas), nth0(1, Somas, Soma), Soma =\= 0), Somas_h),
+    bagof(espaco(Soma, Espaco), Indice^(between(1, N, Indice), nth1(Indice, Vars_Sorted, Espaco), nth1(Indice, Somas_h, Soma)), Espacos), !.
+
+espaco_fila1(Fila, Esp, H_V) :-
+    espacos_fila(H_V, Fila, Espacos), member(Mem, Espacos),
+    Esp = Mem.
+
+% ------------------------------------------------------------------------------
+%numeros_comuns([P | R], Numeros_comuns) :-
+%    length(P, N),
+%    findall((Pos, Candidato), 
+%    (between(1, N, Pos), nth1(_, P, Num), nth1(_, Mem, Candidato),
+%    Num == Candidato),
+%    Numeros_comuns).
+
