476754bf4b1fe2683c68cace3cdd2ccb5365f526
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2021-05-18 16:38:53 +0100

    testes...

--- teste.pl
+++ teste.pl
@@ -1,103 +1,24 @@
-%% -------------------------------------------------------
-aux([P | R], [P | Res]) :-
-    var(P),
-    aux(R, Res).
+% -----------------------------------------------------------------------------------------
 
-aux([P | _], []) :- is_list(P), !.
+Perms_Possiveis =
+        [[[_139542552,_139542558],[[1,2],[2,1]]],
+        [[_139542588,1,_139542600,_139542606],[[7,1,2,6],[7,1,3,5],[7,1,5,3],[7,1,6,2],[8,1,2,5],[8,1,3,4],[8,1,4,3],[8,1,5,2],[9,1,2,4],[9,1,4,2]]],
+        [[_139542636,2,_139542648,7],[[8,2,9,7],[9,2,8,7]]],
+        [[9,8],[[9,8]]],
+        [[_139542588,_139542636,9],[[7,8,9],[8,7,9]]],
+        [[1,2,8],[[1,2,8]]],
+        [[_139542552,_139542600,_139542648],[[1,7,9],[1,9,7],[2,6,9],[2,7,8],[2,8,7],[2,9,6]]],
+        [[_139542558,_139542606,7],[[1,2,7],[2,1,7]]]],
 
-variaveis([P | R], H_V, A, L) :- 
-    var(P), 
-    variaveis(R, H_V, A, L).
+Perms =
+        [[[1,_184,_190],[[1,2,8],[1,3,7],[2,1,8],[2,4,5]]],
+        [[2,_244,_250],[[2,8,9],[2,9,8]]],
+        [[_316,_322,_328],[[5,8,9],[6,7,9],[6,9,7],[8,5,9]]],
+        [[2,5,7],[[2,5,7]]],
+        [[1,2],[[1,2]]],
+        [[_184,_244,_316,2],[[1,2,6,9],[1,2,7,8],[1,2,8,7],[1,2,9,6],[1,3,5,9],[1,3,6,8],[1,3,8,6],[1,3,9,5],[1,4,5,8],[1,4,6,7],[1,4,7,6],[1,4,8,5],[1,5,8,4],[1,5,9,3],[1,6,7,4],[1,6,8,3],[1,6,9,2],[1,7,6,4],[1,7,8,2],[1,8,5,4],[1,8,6,3],[1,8,7,2],[1,9,5,3],[1,9,6,2],[2,3,5,8],[2,3,6,7],[2,3,7,6],[2,3,8,5],[2,3,9,4],[2,4,5,7],[2,4,7,5],[2,4,9,3],[2,5,7,4],[2,5,8,3],[2,6,7,3],[2,6,9,1],[2,7,5,4],[2,7,6,3],[2,7,8,1],[2,8,5,3],[2,8,7,1],[2,9,6,1],[3,2,5,8],[3,2,6,7],[3,2,7,6],[3,2,8,5],[3,2,9,4],[3,4,5,6],[3,4,6,5],[3,4,9,2],[3,5,6,4],[3,5,8,2],[3,5,9,1],[3,6,5,4],[3,6,7,2],[3,6,8,1],[3,7,6,2],[3,8,5,2],[3,8,6,1],[3,9,5,1],[4,2,5,7],[4,2,7,5],[4,2,9,3],[4,3,5,6],[4,3,6,5],[4,3,9,2],[4,5,6,3],[4,5,7,2],[4,5,8,1],[4,6,5,3],[4,6,7,1],[4,7,5,2],[4,7,6,1],[4,8,5,1],[5,2,7,4],[5,2,8,3],[5,3,6,4],[5,3,8,2],[5,3,9,1],[5,4,6,3],[5,4,7,2],[5,4,8,1],[6,2,7,3],[6,2,9,1],[6,3,5,4],[6,3,7,2],[6,3,8,1],[6,4,5,3],[6,4,7,1],[7,2,5,4],[7,2,6,3],[7,2,8,1],[7,3,6,2],[7,4,5,2],[7,4,6,1],[8,2,5,3],[8,2,7,1],[8,3,5,2],[8,3,6,1],[8,4,5,1]]],
+        [[_190,_250,_322,5],[[5,7,8,9],[5,7,9,8],[5,8,7,9],[5,8,9,7],[5,9,7,8],[5,9,8,7],[7,5,8,9],[7,5,9,8],[7,8,5,9],[7,8,9,5],[7,9,5,8],[7,9,8,5],[8,5,7,9],[8,5,9,7],[8,7,5,9],[8,7,9,5],[8,9,5,7],[8,9,7,5]]],
+        [[_328,7],[[7,9],[9,7]]]],
+retira_impossiveis(Perms, New), maplist(writeln, New).
 
-variaveis([P | R], _, [], _) :- 
-    is_list(P), 
-    aux(R, Aux),
-    length(Aux, N), N == 0.
-
-variaveis([P | R], H_V, Aux, V) :- 
-    is_list(P), 
-    aux(R, Aux),
-    length(Aux, N), N > 0,
-    H_V == v, nth0(0, P, V).
-
-variaveis([P | R], H_V, Aux, H) :- 
-    is_list(P), 
-    aux(R, Aux),
-    length(Aux, N), N > 0,
-    H_V == h, nth0(1, P, H).
-
-espaco_fila(Fila, [], H_V) :-
-    variaveis(Fila, H_V, Aux, _),
-    Aux == [].
-
-espaco_fila(Fila, Esp, H_V) :-
-    variaveis(Fila, H_V, Aux, L),
-    Esp = espaco(L, Aux).
-
-%% ---------------------------------------------------------
-
-pos_variaveis(Fila, Posicoes) :-
-    length(Fila, N),
-    findall([Pos, Candidato], 
-    (between(1, N, Pos), nth1(Pos, Fila, Candidato), is_list(Candidato)),
-    Posicoes).
-
-espaco(Fila, Posicoes, Res) :- espaco(Fila, Posicoes, Res, []).
-
-espaco(_, [], Aux, Aux).
-
-espaco(Fila, [[El, _]], Res, Aux) :- 
-    length(Fila, Max), Min is El + 1,
-    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
-    espaco(Fila, [], Res, [Vars | Aux]).
-
-espaco(Fila, [El1, El2 | R], Res, Aux) :-
-    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
-    Min is Pos1 + 1, Max is Pos2 - 1,
-    Min < Max,
-    bagof(Membro, Indice^(between(Min, Max, Indice), nth1(Indice, Fila, Membro)), Vars),
-    append([El2], R, New_R),
-    espaco(Fila, New_R, Res, [Vars | Aux]).
-
-espaco(Fila, [El1, El2 | R], Res, Aux) :-
-    nth0(0, El1, Pos1), nth0(0, El2, Pos2),
-    Min is Pos1 + 1, Max is Pos2 - 1,
-    Min >= Max,
-    append([El2], R, New_R),
-    espaco(Fila, New_R, Res, Aux).
-
-espacos_fila(H_V, Fila, Espacos) :-
-    pos_variaveis(Fila, Posicoes),
-    espaco(Fila, Posicoes, Vars), length(Vars, N), sort(Vars, Vars_Sorted),
-    H_V == v,
-    findall(Soma, (member(Mem, Posicoes), nth0(1, Mem, Somas), nth0(0, Somas, Soma), Soma =\= 0), Somas_v),
-    bagof(espaco(Soma, Espaco), Indice^(between(1, N, Indice), nth1(Indice, Vars_Sorted, Espaco), nth1(Indice, Somas_v, Soma)), Espacos), !.
-
-espacos_fila(H_V, Fila, Espacos) :-
-    pos_variaveis(Fila, Posicoes),
-    espaco(Fila, Posicoes, Vars), length(Vars, N), sort(Vars, Vars_Sorted),
-    H_V == h,
-    findall(Soma, (member(Mem, Posicoes), nth0(1, Mem, Somas), nth0(1, Somas, Soma), Soma =\= 0), Somas_h),
-    bagof(espaco(Soma, Espaco), Indice^(between(1, N, Indice), nth1(Indice, Vars_Sorted, Espaco), nth1(Indice, Somas_h, Soma)), Espacos), !.
-
-espaco_fila1(Fila, Esp, H_V) :-
-    espacos_fila(H_V, Fila, Espacos), member(Mem, Espacos),
-    Esp = Mem.
-
-% ------------------------------------------------------------------------------
-Puzzle = [[[0, 0], [0, 0], [0, 0], [17, 0], [10, 0]],
-		 [[0, 0], [24, 0], [11, 3], P24, P25], 
-         [[0,16], P32, P33, P34, P35],
-		 [[0,26], P42, P43, P44, P45],
-         [[0,17], P52, P53, [0,0], [0,0]]],
-         espacos_puzzle(Puzzle, Espacos),
-nth1(1, Espacos, Esp1),
-espacos_com_posicoes_comuns(Espacos, Esp1, Esps_com).
-
-Espacos = [espaco(3, [P24, P25]), espaco(16, [P32, P33, P34, P35]), 
-        espaco(26, [P42, P43, P44, P45]), espaco(17, [P52, P53]),
-        espaco(24, [P32, P42, P52]), espaco(11, [P33, P43, P53]), 
-        espaco(17, [P24, P34, P44]), espaco(10, [P25, P35, P45])],
-Esp1 = espaco(3, [P24, P25]),
-espacos_com_posicoes_comuns(Espacos, Esp1, Esps_com).
 
